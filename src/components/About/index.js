d8fa8bbbdca3333d79b6d8f05825ecd52228260f
sudheerkutha <sudheerreddy9010@gmail.com>
2023-12-07 12:42:57 +0000

    final output

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+// Write your JS code here
+const About = () => (
+  <div className="AboutCotainer">
+    <img
+      className="Image"
+      alt="AboutImage"
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+    />
+    <h1 className="Heading">About</h1>
+    <p>I Have to create i am a frontend Developer!</p>
+  </div>
+)
+export default About
